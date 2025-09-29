7673999780077b51f47dcc9601aefa90d7f4f6ea
cyber-steer <cybersteer@naver.com>
2025-09-30 00:40:29 +0900

    refactor: User Object

--- server/app/domains/user/crud.py
+++ server/app/domains/user/crud.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import Session
 from app.domains.user.models import User
 
-def create_user(db: Session, user_instance: User):
-    # user = User(**user_data)
+def create_user(db: Session, user: User):
     db.add(user)
     db.commit()
     db.refresh(user)
